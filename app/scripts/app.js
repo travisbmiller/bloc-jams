d48050b8b0bb713e05012945ff1bf04a1501f21d
Travis Miller <travismiller@Traviss-MacBook-Air.local>
2014-07-08 13:50:16 -0700

    finshed checkpoint

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  console.log("Landing.controller");
+  $scope.subText = "Turn the music up!";
+}]);
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,4 +4,28 @@
 
 angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
   console.log("Landing.controller");
+ $scope.blocTitle = "Bloc Jams";
+ $scope.subText = "Turn the music up!";
+ 
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+ 
+  $scope.shufflePictures = function(o) {
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+ $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+
  }]);
