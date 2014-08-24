6e0895a83f526684da7c1624236be3e065b5a44a
Travis Miller <travisbmiller@outlook.com>
2014-08-24 18:12:26 +0800

    checkpoint completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,11 +5,11 @@ var albumPicasso = {
   year: '1881',
   albumArtUrl: 'images/album-placeholder.png',
   songs: [
-      { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
     ]
 };
 
@@ -212,6 +212,11 @@ angular
       this.playing = false;
       currentSoundFile.pause();
     },
+    seek: function(time) {
+      if (currentSoundFile) {
+        currentSoundFile.setTime(time);
+      }
+    },
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -264,21 +269,47 @@ angular
       return offsetXPercent;
     }
     
+    var numberFromValue = function (value, defaultValue) {
+      if (typeof value === 'number') {
+        return value;
+      } 
+    
+      if ( typeof value === 'undefined') {
+        return defaultValue;
+      }
+
+      if (typeof value === 'string') {
+        return Number(value);
+      }
+
+    };
 
     return {
       templateUrl: '/templates/directives/slider.html', 
       replace: true,
       restrict: 'E',
-      scope: {},
+      scope: {
+        onChange: '&'
+      },
       link: function(scope, element, attributes) {
         
         scope.value = 0,
-        scope.max = 200;
+        scope.max = 100;
+
+        attributes.$observe('value', function(newValue){
+          scope.value = numberFromValue(newValue, 0);
+        });
+
+        attributes.$observe('max', function(newValue){
+          scope.max = numberFromValue(newValue, 100) || 100;
+        });
 
         var $seekBar = $(element);
 
         var percentString = function () {
-          percent = Number(scope.value) / Number(scope.max) * 100;
+          var value = scope.value || 0;
+          var max = scope.max || 0;
+          percent = value / max * 100;
           return percent + '%';
         }
 
@@ -293,6 +324,7 @@ angular
         scope.onClickSlider = function(event) {
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.value = percent * scope.max;
+          notifyCallback(scope.value);
         }  
 
         
@@ -303,6 +335,7 @@ angular
            
            scope.$apply(function() {
             scope.value = percent * scope.max;
+            notifyCallback(scope.value);
            });
             
 
@@ -315,6 +348,13 @@ angular
            $document.off('mouseup.thumb');
          });
        };
+
+       var notifyCallback = function (newValue) {
+        if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+       };
+
       }
     };
   }]);
